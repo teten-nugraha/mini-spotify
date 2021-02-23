907068a6a1d6d3cfe852672455d2d7c766a55e57
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-23 10:55:51 +0700

    install fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bulma/css/bulma.css'
+
+import FontAwesome from '@fortawesome/fontawesome'
+import VueFontAwesome from '@fortawesome/vue-fontawesome'
+import FontAwesomeSolid from '@fortawesome/fontawesome-free-solid'
+
+FontAwesome.library.add(FontAwesomeSolid)
+
+Vue.component('fa-icon', VueFontAwesome)
+
 Vue.config.productionTip = false
 
 new Vue({
