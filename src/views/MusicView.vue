1a09e16c39746392ed488336f82b7f89a5be70e4
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-23 11:24:11 +0700

    list and order

---
+++ src/views/MusicView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MusicComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MusicComponent from '@/components/MusicComponent.vue'
+
+export default {
+  name: 'MusicView',
+  components: {
+    MusicComponent
+  }
+}
+</script>
